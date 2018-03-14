55dd433f9d3f294b98f0682d3bfbff8a129b6937
eddyspreeuwers <e.spreeuwers@gmail.com>
2018-03-14 08:39:59 +0100

    fixed index

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,2 @@
-/// <reference types="xml-parser" />
-/***********
-generated template classes for ./test/importedClass.xsd 2018-3-14 08:33:20
-***********/
-import * as dep from "xml-parser";
-export declare class Test {
-    firstName: string;
-    lastName?: string;
-    imported: dep.Node;
-    constructor(props?: Test);
-}
+/// <reference types="lodash" />
+export declare function generateTemplateClassesFromXSD(xsdFilePath: string, dependencies?: Map<string, string>): void;
